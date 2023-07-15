5e35efae9d86090128ebfd38cfbf920ff17d3bb3
jihyun29 <128969324+jihyun29@users.noreply.github.com>
2023-07-15 12:26:30 +0900

    로컬테스트완료p2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 import * as mediasoupClient from "mediasoup-client";
 
 const App = () => {
+  // const [force, setForce] = useState(false);
   const localVideoRef = useRef(null);
   const remoteVideoRef = useRef(null);
   let device;
@@ -11,7 +12,7 @@ const App = () => {
   let consumerTransport;
   let producer;
   let consumer;
-  const websocketURL = "http://localhost:3001";
+  const websocketURL = "https://localhost:3001";
   let params = {
     // mediasoup params
     encodings: [
@@ -225,47 +226,43 @@ const App = () => {
   const createRecvTransport = async () => {
     // see server's socket.on('consume', sender?, ...)
     // this is a call from Consumer, so sender = false
-    await socket.emit(
-      "createWebRtcTransport",
-      { sender: false },
-      ({ params }) => {
-        // The server sends back params needed
-        // to create Send Transport on the client side
-        if (params.error) {
-          console.log(params.error);
-          return;
-        }
-
-        console.log(params);
-
-        // creates a new WebRTC Transport to receive media
-        // based on server's consumer transport params
-        // https://mediasoup.org/documentation/v3/mediasoup-client/api/#device-createRecvTransport
-        consumerTransport = device.createRecvTransport(params);
-
-        // https://mediasoup.org/documentation/v3/communication-between-client-and-server/#producing-media
-        // this event is raised when a first call to transport.produce() is made
-        // see connectRecvTransport() below
-        consumerTransport.on(
-          "connect",
-          async ({ dtlsParameters }, callback, errback) => {
-            try {
-              // Signal local DTLS parameters to the server side transport
-              // see server's socket.on('transport-recv-connect', ...)
-              await socket.emit("transport-recv-connect", {
-                dtlsParameters,
-              });
-
-              // Tell the transport that parameters were transmitted.
-              callback();
-            } catch (error) {
-              // Tell the transport that something was wrong
-              errback(error);
-            }
-          }
-        );
+    socket.emit("createWebRtcTransport", { sender: false }, ({ params }) => {
+      // The server sends back params needed
+      // to create Send Transport on the client side
+      if (params.error) {
+        console.log(params.error);
+        return;
       }
-    );
+
+      console.log(params);
+
+      // creates a new WebRTC Transport to receive media
+      // based on server's consumer transport params
+      // https://mediasoup.org/documentation/v3/mediasoup-client/api/#device-createRecvTransport
+      consumerTransport = device.createRecvTransport(params);
+
+      // https://mediasoup.org/documentation/v3/communication-between-client-and-server/#producing-media
+      // this event is raised when a first call to transport.produce() is made
+      // see connectRecvTransport() below
+      consumerTransport.on(
+        "connect",
+        async ({ dtlsParameters }, callback, errback) => {
+          try {
+            // Signal local DTLS parameters to the server side transport
+            // see server's socket.on('transport-recv-connect', ...)
+            await socket.emit("transport-recv-connect", {
+              dtlsParameters,
+            });
+
+            // Tell the transport that parameters were transmitted.
+            callback();
+          } catch (error) {
+            // Tell the transport that something was wrong
+            errback(error);
+          }
+        }
+      );
+    });
   };
 
   const connectRecvTransport = async () => {
@@ -282,7 +279,7 @@ const App = () => {
           console.log("Cannot Consume");
           return;
         }
-
+        console.log("여기::: ? ? ");
         console.log(params);
         // then consume with the local consumer transport
         // which creates a consumer
@@ -296,11 +293,15 @@ const App = () => {
         // destructure and retrieve the video track from the producer
         const { track } = consumer;
 
-        remoteVideoRef.srcObject = new MediaStream([track]);
+        console.log("consumer", consumer);
+        console.log("remoteVideoRef", remoteVideoRef);
+
+        remoteVideoRef.current.srcObject = new MediaStream([track]);
 
         // the server consumer started with media paused
         // so we need to inform the server to resume
         socket.emit("consumer-resume");
+        // setForce((prev) => !prev);
       }
     );
   };
@@ -414,6 +415,7 @@ const App = () => {
             <td>
               <div id="sharedBtns">
                 <video
+                  style={{ border: "1px solid black" }}
                   ref={remoteVideoRef}
                   autoPlay
                   playsInline
